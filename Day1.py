e22ad119c360ea0277ff6417e7ef054840e2ec54
Sanyam Kunwar <sanyamkunwar@Sanyams-MacBook-Pro.local>
2025-01-09 22:54:40 +0530

    edit commit

--- Day1.py
+++ Day1.py
@@ -1,6 +1,24 @@
-print('Its my first Day in Python. So just setting up the environment and getting started with the basics.')
-print('I am excited to learn and explore the world of Python.')
-print('I am ready to take on the challenges and make the most out of this experience.')
-print('I am looking forward to the journey ahead and the knowledge that I will gain.')
-print('I am ready to take on the challenges and make the most out of this experience.')
-print('I am looking forward to the journey ahead and the knowledge that I will gain.')
+# print('Its my first Day in Python. So just setting up the environment and getting started with the basics.')
+# print('I am excited to learn and explore the world of Python.')
+# print('I am ready to take on the challenges and make the most out of this experience.')
+# print('I am looking forward to the journey ahead and the knowledge that I will gain.')
+# print('I am ready to take on the challenges and make the most out of this experience.')
+# print('I am looking forward to the journey ahead and the knowledge that I will gain.\n')
+
+#==============================================================================================
+# count = 0
+# # even numbers between 1 to 10
+# for i in range(1,10):
+#     if i%2==0:
+#         count+=1
+#         print(i)
+
+# print('Total even numbers between 1 to 10 are:',count)
+#==============================================================================================
+# letters = ['a','b','c','d','e','f','g','h','i','j']
+# items = [0,"a"]
+# index,letter = items
+# for index,letter in enumerate(letters):
+#     print(index,letter)
+#==============================================================================================
+
