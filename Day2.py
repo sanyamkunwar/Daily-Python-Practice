f8fb34cb4d01fc72dca7035da980e269442a1add
Sanyam Kunwar <sanyamkunwar@Sanyams-MacBook-Pro.local>
2025-01-10 22:34:37 +0530

    day 2

--- Day2.py
+++ Day2.py
@@ -8,3 +8,34 @@ def multiply(*args):
 
 
 print(multiply(1,2,3,4,5,6,7,8,9,10))
+#================================================================================================
+
+def save_user(**user):
+    print(user)
+
+save_user(id=1,name='John',age=22)
+
+#================================================================================================
+
+def fizz_buzz(input):
+    if input%3==0 and input%5==0:
+        return 'FizzBuzz'
+    if input%3==0:
+        return 'Fizz'
+    if input%5==0:
+        return 'Buzz'
+    return input
+
+print(fizz_buzz(15))
+
+#================================================================================================
+
+message = 'a'
+def greet():
+    global message
+    message = 'b'
+
+greet()
+print(message)
+
+#================================================================================================
