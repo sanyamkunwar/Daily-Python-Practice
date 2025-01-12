6de30d2b056141956fd2ae5c5a2e3f1c1a8caeaf
Sanyam Kunwar <sanyamkunwar2002@gmail.com>
2025-01-12 09:54:20 +0530

    day4

--- Day4.py
+++ Day4.py
@@ -16,5 +16,42 @@
 # Hints
 # Using \n (newline) and \t (tab) to format the string
 
-print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!")
+# print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!")
 
+# def mot(b):
+#     a=[]
+#     for i in range(len(b)-1):
+#         while b[i] not in a:
+#             a+=b[i]
+#             i=i+1
+#         else:
+#             break
+#     return a       
+
+# print(mot("abcbb"))
+# print(mot('hgdjuhg'))
+import numpy as np
+def mot(b):
+    a=np.zeros(len(b))
+    for j in range(0,len(b)-2):
+        for i in range(len(b)-1):
+            while b[i] not in a:
+                a[i,:]=b[i]
+                i=i+1
+            else:
+                break
+        j+=1        
+        return a  
+    
+# print(mot("abcbb"))
+
+a=np.mat(8)
+print(a)
+
+def matrice(i,j): return [[0 for q in range(0,j)] for p in range(0,i)]
+r=matrice(7,7)
+print(r)
+
+r[1]+='1'
+r[1]
+print(r)
